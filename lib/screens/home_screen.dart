95905bc4f4ca418452c7f168e414764f9e9cf031
Abdelilah-Falih <121343981+Abdelilah-Falih@users.noreply.github.com>
2023-07-01 01:19:06 +0100

    task 02 : add package faker ! 	modified:   lib/screens/home_screen.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,13 @@
+import 'package:faker_dart/faker_dart.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   HomePage();
+  final faker = Faker.instance;
 
   @override
   Widget build(BuildContext context) {
+    String user_name = faker.name.fullName();
     return Scaffold(
         appBar: AppBar(
           title: Text("Welcome"),
@@ -12,19 +15,21 @@ class HomePage extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Container(
-            width: double.infinity,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), 
             color: Color.fromARGB(255, 169, 217, 249),
+            ),
+            width: double.infinity,
             child: Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(15.0),
               child: Column(
                 children: [
                   Row(
                     children: [
                       const Text("Hi ",
                           style: TextStyle(fontWeight: FontWeight.bold)),
-                      const Text(
-                        " Mr.Falih",
-                        style: TextStyle(
+                      Text(
+                        user_name,
+                        style: const TextStyle(
                           fontSize: 20,
                         ),
                       ),
